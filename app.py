4acad10e9ac5902c0b08fc7dcbddda4110118b7f
Rishikesh <rishikeshjagadale19@gmail.com>
2022-09-28 22:01:07 +0530

    Update commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #
+
 import json
 import pickle
 
@@ -26,12 +27,14 @@ def predict_api():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data = [float(x) for x in request.form.values()]
-    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))    
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 
 
 if __name__=="__main__":
     app.run(debug=True)
+    
